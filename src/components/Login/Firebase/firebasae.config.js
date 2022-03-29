3adfb8cfe55b4dd3f74188f513ec679a3114b8bd
ixaman <63200084+ixaman@users.noreply.github.com>
2022-03-29 14:13:00 +0600

    first commit

---
+++ src/components/Login/Firebase/firebasae.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqb6Lv3GE5wtX3zaLJhx0wpoCpHiVp1Q8",
+    authDomain: "elearning-platform-2ecaf.firebaseapp.com",
+    projectId: "elearning-platform-2ecaf",
+    storageBucket: "elearning-platform-2ecaf.appspot.com",
+    messagingSenderId: "20131966156",
+    appId: "1:20131966156:web:881125d523db92384ee669"
+  };
+
+  export default firebaseConfig;
